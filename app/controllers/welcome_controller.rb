ed0c3f82d2ab0a0edfa550256dc7a96cdf9e8d36
Max Grønlund <max@synthmax.dk>
2016-08-12 09:57:33 +0200

    some crapy code added

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
-  def index
+  def index 
+    'jkldsfjkdfsjkldsfjkljkdflskjldsfjkldsfjkdfsjkdsfjkljklsdfjdsfjldfsjklfdslkjfdsjlfdsjljklsdfjklfsdjlkdsfjlklsdjkf'
   end
 end
